9fce80154f7de5cd90a1710e8af808eda3d8b498
Long Bao <mglongbao@gmail.com>
2024-06-26 11:25:15 +0700

    Fix Login to get from DB

--- DiamondAssessment/Pages/Login.cshtml.cs
+++ DiamondAssessment/Pages/Login.cshtml.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.Models.Enum;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Service.Abstractions;
 
 namespace DiamondAssessment.Pages;
 
 public class Login : PageModel
 {
+    private readonly IStaffService _staffService;
+
+    public Login(IStaffService staffService)
+    {
+        _staffService = staffService;
+    }
+
     [BindProperty]
-    public string Username { get; set; }
-    
+    public string Email { get; set; }
+
     [BindProperty]
     public string Password { get; set; }
 
@@ -15,46 +24,51 @@ public class Login : PageModel
     {
         try
         {
-            //var loginAccount = _systemAccountService.Login(Username, Password);
-            var loginAccount = new
-            {
-                AccountId = 1,
-                AccountEmail = "mail@mail.com",
-                AccountRole = 3
-            };
-            if (Username == "admin")
-            {
-                loginAccount = new
-                {
-                    AccountId = 1,
-                    AccountEmail = "mail@mail.com",
-                    AccountRole = 1
-                };
-            } else if (Username == "manager")
-            {
-                loginAccount = new
-                {
-                    AccountId = 1,
-                    AccountEmail = "mail@mail.com",
-                    AccountRole = 2
-                };
-            }
+            var loginAccount = _staffService
+                .FindByCondition(x => x.Email == Email && x.Password == Password, false)
+                .FirstOrDefault();
+            // var loginAccount = new
+            // {
+            //     AccountId = 1,
+            //     AccountEmail = "mail@mail.com",
+            //     AccountRole = 3
+            // };
+            // if (Username == "admin")
+            // {
+            //     loginAccount = new
+            //     {
+            //         AccountId = 1,
+            //         AccountEmail = "mail@mail.com",
+            //         AccountRole = 1
+            //     };
+            // }
+            // else if (Username == "manager")
+            // {
+            //     loginAccount = new
+            //     {
+            //         AccountId = 1,
+            //         AccountEmail = "mail@mail.com",
+            //         AccountRole = 2
+            //     };
+            // }
             System.Diagnostics.Debug.WriteLine(loginAccount);
-            HttpContext.Session.SetString("AccountId", loginAccount.AccountId.ToString());
-            HttpContext.Session.SetString("AccountEmail", loginAccount.AccountEmail!);
-            HttpContext.Session.SetString("Role", loginAccount.AccountRole.ToString()!);
-            switch (loginAccount.AccountRole)
+            // HttpContext.Session.SetString("AccountId", loginAccount.AccountId.ToString());
+            // HttpContext.Session.SetString("AccountEmail", loginAccount.AccountEmail!);
+            // HttpContext.Session.SetString("Role", loginAccount.AccountRole.ToString()!);
+            HttpContext.Session.SetString("AccountId", loginAccount.Id.ToString());
+            HttpContext.Session.SetString("AccountEmail", loginAccount.Email!);
+            HttpContext.Session.SetString("Role", loginAccount.Role.ToString()!);
+            switch (loginAccount.Role)
             {
-                case 1:
+                case Role.Admin:
                     Response.Redirect("/Admin/Account/Index");
                     break;
-                case 2:
+                case Role.Manager:
                     Response.Redirect("/Manager/AssessmentPaper/Index");
                     break;
-                case 3:
+                case Role.Staff:
                     Response.Redirect("/Staff/RegisterForm/FormPage");
                     break;
-                
             }
         }
         catch (Exception e)
