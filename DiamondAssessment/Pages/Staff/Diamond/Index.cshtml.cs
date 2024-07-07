25aaa91afddb35aa167bf75fb507d481b0582d2c
Long Bao <mglongbao@gmail.com>
2024-07-07 15:53:09 +0700

    Update Diamond UI, Paging, add update and delete

--- DiamondAssessment/Pages/Staff/Diamond/Index.cshtml.cs
+++ DiamondAssessment/Pages/Staff/Diamond/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Service.Abstractions;
 
@@ -8,8 +9,24 @@ public class IndexModel(IDiamondDetailService diamondService) : PageModel
 {
     public IEnumerable<DiamondDetail> Diamonds { get; set; }
 
-    public void OnGet()
+    [BindProperty(SupportsGet = true)]
+    public int PageNumber { get; set; } = 1;
+    public int TotalPages { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
     {
-        Diamonds = diamondService.GetAllDiamondDetailsAsync().Result;
+        var diamonds = await diamondService.GetAllDiamondDetailsAsync();
+        var totalDiamonds = diamonds.Count();
+        TotalPages = (int)Math.Ceiling(totalDiamonds / 2.0);
+
+        if (PageNumber < 1)
+            PageNumber = 1;
+
+        if (PageNumber > TotalPages)
+            PageNumber = TotalPages;
+
+        Diamonds = diamonds.Skip((PageNumber - 1) * 2).Take(2).ToList();
+
+        return Page();
     }
 }
