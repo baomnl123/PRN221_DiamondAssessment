a971bad69cc3f0c0d6e79fad45a06e8f931b768f
TRUNG VIET <TRUNG VIET@TRUNGVIET>
2024-07-13 19:54:38 +0700

    update tao cf

--- DiamondAssessment/Pages/Manager/CommitmentForm/Create.cshtml.cs
+++ DiamondAssessment/Pages/Manager/CommitmentForm/Create.cshtml.cs
@@ -11,23 +11,29 @@ namespace DiamondAssessment.Pages.Manager.CommitmentForm
     public class CreateModel : PageModel
     {
         private readonly ICommitmentFormService _commitmentFormService;
-
-        public CreateModel(ICommitmentFormService commitmentFormService)
+        private readonly IAssessmentPaperService _assessmentPaperService;
+        public CreateModel(ICommitmentFormService commitmentFormService, IAssessmentPaperService assessmentPaperService)
         {
             _commitmentFormService = commitmentFormService;
+            _assessmentPaperService = assessmentPaperService;
         }
         [BindProperty]
         public Entities.Models.CommitmentForm CommitmentForm { get; set; }
-        public async Task<IActionResult> OnGetAsync()
+        public Entities.Models.AssessmentPaper AssessmentPaper { get; set; }
+        public async Task<IActionResult> OnGetAsync(string? paperId)
         {
+            //var paper = _assessmentPaperService
+            //.FindByCondition(f => f.Id == Guid.Parse(paperId), false)
+            //.FirstOrDefault();
+            //AssessmentPaper = paper;
             return Page();
 
         }
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string? paperId)
         {
-            CommitmentForm.Id = new Guid();
+                     
             CommitmentForm.IsDelete = false;
-            
+            CommitmentForm.PaperId = Guid.Parse(paperId);
             await _commitmentFormService.Create(CommitmentForm);
             return RedirectToPage("/Manager/CommitmentForm/Index");
 
