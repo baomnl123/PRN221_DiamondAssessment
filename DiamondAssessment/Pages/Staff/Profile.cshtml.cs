91e43cad4a4f64c87dabc77fe258ecfeb982d3a1
TRUNG VIET <TRUNG VIET@TRUNGVIET>
2024-07-14 21:32:17 +0700

    update profile

--- DiamondAssessment/Pages/Staff/Profile.cshtml.cs
+++ DiamondAssessment/Pages/Staff/Profile.cshtml.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Client;
+using Service.Abstractions;
 
 namespace DiamondAssessment.Pages.Staff;
 
 public class Profile : PageModel
 {
-    public void OnGet()
+    private readonly IStaffService _staffService;
+
+    public Profile(IStaffService staffService)
     {
-        
+        _staffService = staffService;
+    }
+    [BindProperty]
+    public Entities.Models.Staff Staff { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var id = HttpContext.Session.GetString("AccountId");
+        Staff = _staffService.FindByCondition(s => s.Id == Guid.Parse(id) , false).FirstOrDefault();
+        return Page();
     }
 }
