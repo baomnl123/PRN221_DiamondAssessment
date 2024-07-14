92287dcd8baf0661d98de099408cc83c10754f7f
phatnh13 <phatnhse171208@fpt.edu.vn>
2024-07-14 20:47:21 +0700

    <fix> create sealing form

--- DiamondAssessment/Pages/Manager/SealingReport/Create.cshtml.cs
+++ DiamondAssessment/Pages/Manager/SealingReport/Create.cshtml.cs
@@ -1,44 +1,60 @@
+using Entities.Models;
 using Entities.Models.Enum;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Service.Abstractions;
 
 namespace DiamondAssessment.Pages.Manager.SealingReport
 {
-    public class CreateModel : PageModel
+    public class Create : PageModel
     {
         private readonly ISealingReportService _sealingReportService;
+        private readonly IAssessmentPaperService _assessmentPaperService;
 
-        [BindProperty(SupportsGet = true)] public Guid paperId { get; set; }
-        public CreateModel(ISealingReportService sealingReportService)
+        public Create(IAssessmentPaperService assessmentPaperService, ISealingReportService sealingReportService)
         {
+            _assessmentPaperService = assessmentPaperService;
             _sealingReportService = sealingReportService;
         }
 
+        [BindProperty(SupportsGet = true)]
+        public Guid PaperId { get; set; }
+
+        [BindProperty]
         public Entities.Models.SealingReport SealingReport { get; set; }
-        public async Task<IActionResult> OnGetAsync(Guid paperId)
+        
+
+        public async Task<IActionResult> OnGetAsync(Guid PaperId)
         {
-            // Pre-populate the form with the PaperId and the current date.
+            if (PaperId == Guid.Empty)
+            {
+                return NotFound();
+            }
+
             SealingReport = new Entities.Models.SealingReport
             {
-                PaperId = paperId,
+                PaperId = PaperId,
                 CreatedAt = DateTime.UtcNow,
                 IsDelete = false
             };
+
             return Page();
         }
-        public async Task<IActionResult> OnPostAsync()
+
+        public async Task<IActionResult> OnPostAsync(Guid PaperId)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-            
-            SealingReport.ModifiedAt = DateTime.UtcNow;
+            SealingReport.Id = new Guid();
             SealingReport.CreatedAt = DateTime.UtcNow;
+            SealingReport.ModifiedAt = DateTime.UtcNow;
             SealingReport.IsDelete = false;
-            SealingReport.PaperId = paperId;
+            SealingReport.PaperId = PaperId;
             SealingReport.SealingReportStatus = SealingReportStatus.Pending;
+
             await _sealingReportService.Create(SealingReport);
 
             return RedirectToPage("/Staff/AssessmentPaper/Index");
